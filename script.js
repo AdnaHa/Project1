3718585825b76d3733eb95b90c899a4925122b6b
AdnaHa <48633775+AdnaHa@users.noreply.github.com>
2019-11-24 23:43:04 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+function runCode1() { 
+    alert ("Soon to be announced")
+}
+function runCode1() { 
+    alert ("Soon to be announced")
+}
+function runCode2() { 
+    alert ("Soon to be announced")
+}
